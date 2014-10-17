dac7fc3644cef53af5e5ed6fdf2bc01be4fb839c
toshko07 <t.tabakov94@gmail.com>
2014-10-17 19:27:38 +0300

    First problem

--- week1/1-fmi-tribute/solution.rb
+++ week1/1-fmi-tribute/solution.rb
@@ -1,3 +1,23 @@
 class Array
-  # Your code goes here.
+  def to_hash
+    Hash[*self.flatten]
+  end
+
+  def index_by
+    lastwords_array = self.map { |arg| yield(arg) }
+    zipped_array = lastwords_array.zip(self)
+    Hash[*(zipped_array.flatten)]
+  end
+
+  def subarray_count(arr)
+    count = 0
+    new_arr = self.each_cons(arr.size)
+    new_arr.each {|el| count += 1 if arr == el }
+    count
+  end
+
+  def occurences_count
+    self.reduce(Hash.new(0)) { |total, e| total[e] += 1 ;total}
+  end
+
 end
--- week1/1-fmi-tribute/solution_test.rb
+++ week1/1-fmi-tribute/solution_test.rb
@@ -10,11 +10,9 @@ class ArrayTest < Minitest::Test
   def test_index_by
     assert_equal({'Coltrane' => 'John Coltrane', 'Davis' => 'Miles Davis' }, ['John Coltrane', 'Miles Davis'].index_by { |name| name.split(' ').last })
   end
-
   def test_occurences_count
     assert_equal({foo: 2, bar: 1}, [:foo, :bar, :foo].occurences_count)
   end
-
   def test_subarray_count
     assert_equal 2, [1, 2, 3, 2, 3, 1].subarray_count([2, 3])
   end
