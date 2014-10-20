2f28ebb20b8bb7a2b96c82ee7d59b768b379582d
toshko07 <t.tabakov94@gmail.com>
2014-10-20 18:07:59 +0300

    Hashes solutions + tests

--- week1/2-hashes/solution.rb
+++ week1/2-hashes/solution.rb
@@ -1,3 +1,16 @@
 class Hash
-  # Your code goes here.
+  def pick(*keys)
+  self.select {|key| keys.include? key}
+  end
+
+    def except(*keys)
+    self.select {|key|  !keys.include? key }
+  end
+
+    def compact_values
+    self.select {|key, value| value }
+  end
+    def defaults(hash)
+    self.merge!(hash) {|key,v1,v2| v1}
+  end
 end
--- week1/2-hashes/solution_test.rb
+++ week1/2-hashes/solution_test.rb
@@ -6,4 +6,20 @@ class HashTest < Minitest::Test
   def test_the_truth
     assert true
   end
+
+  def test_pick
+    assert_equal({:a=>1, :b=>2}, {a: 1, b: 2, c: 3}.pick(:a, :b))
+  end
+
+  def test_except
+    assert_equal({:a=>1, :b=>2}, {a: 1, b: 2, d: nil}.except(:d))
+  end
+
+  def test_compact_values
+    assert_equal({:a=>1, :b=>2}, {a: 1, b: 2, c: false, d: nil}.compact_values)
+  end
+
+  def test_defaults
+    assert_equal({:a=>1, :b=>2, :c=>3}, {a: 1, b: 2}.defaults(a: 4, c: 3))
+  end
 end
