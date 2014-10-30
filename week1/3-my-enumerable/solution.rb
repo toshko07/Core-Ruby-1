53cbbda68dfcacedc3eb6eb186693d2ff7b90f99
toshko07 <t.tabakov94@gmail.com>
2014-10-30 16:10:40 +0200

    My enumerable solutions and tests added

--- week1/3-my-enumerable/solution.rb
+++ week1/3-my-enumerable/solution.rb
@@ -1,41 +1,159 @@
 module MyEnumerable
+
   def map
-    # Your code goes here.
+    return to_enum(:map) unless block_given?
+    result = []
+    each { |element| result << yield(element) }
+    result
   end
 
   def filter
-    # Your code goes here.
+    return to_enum(:filter) unless block_given?
+    result = []
+    each { |element| result << element if yield(element) }
+    result
   end
-
   def reject
-    # Your code goes here.
+    return to_enum(:reject) unless block_given?
+    result = []
+    each { |element| result << element unless yield(element) }
+    result
   end
-
   def reduce(initial = nil)
-    # Your code goes here.
-  end
-
+   each { |value| initial = yield(initial, value) }
+   initial
+end
   def any?
-    # Your code goes here.
+    each { |element| return true if yield(element) }
+    false
   end
 
+  def one?
+    match_counter = 0
+    each { |element| match_counter += 1 if yield(element) }
+    match_counter == 1
+  end
   def all?
-    # Your code goes here.
+    each { |element| return false unless yield(element) }
+    true
   end
 
   def each_cons(n)
-    # Your code goes here.
+    return to_enum(:each_cons) unless block_given?
+    result = []
+    each do |element|
+    n.times { result << yield(element) }
+    end
+    result
   end
-
   def include?(element)
-    # Your code goes here.
+    each { |el| return true if el == element }
+    false
   end
-
   def count(element = nil)
-    # Your code goes here.
+    counter = 0
+
+    if block_given?
+      each { |value| counter += 1 if yield(value) }
+      counter
+    elsif element == nil
+      each { |value| counter += 1 }
+      counter
+    elsif element.kind_of?(Integer)
+      each { |value| counter += 1 if value == element }
+      counter
+    end
+  end
+  def size
+    counter = 0
+    each { |element| counter += 1 }
+    counter
+  end
+   def group_by
+    return to_enum(:group_by) unless block_given?
+    result = Hash.new { |h,k| h[k] = [] }
+    each { |el| result[yield(el)] << el }
+    Hash[result.sort]
+   end
+
+  def min
+    result = 1.0 / 0.0
+  if block_given?
+    each { |item| result = item if yield(item) <= result }
+  else
+    each { |item| result = item if item <= result }
+  end
+  result
   end
 
-  def size
-    # Your code goes here.
+  def min_by
+    return to_enum(:min_by) unless block_given?
+    result = 1.0 / 0.0
+    each { |item| result = item if yield(item) <= result }
+    result
   end
+  def max
+    result = -1.0 / 0.0
+    if block_given?
+      each { |item| result = item if yield(item) >= result }
+    else
+      each { |item| result = item if item >= result }
+    end
+    result
+  end
+
+  def max_by
+    return to_enum(:max_by) unless block_given?
+    result = 1.0 / 0.0
+    each { |item| result = item if yield(item) >= result}
+    result
+  end
+
+  def minmax
+    [min, max]
+  end
+
+  def minmax_by
+    return to_enum(:minmax_by) unless block_given?
+    [min_by, max_by]
+  end
+
+  def take(n)
+    result = []
+    each  do |element|
+      result << element
+     return result if result.size == n
+    end
+    result
+  end
+
+  def take_while
+    return to_enum(:take_while) unless block_given?
+    result = []
+    each { |element| yield(element)? result << element : false }
+    result
+  end
+
+  def drop(n)
+    result  = []
+    counter = 0
+    each do |element|
+      counter += 1
+      result << element unless counter <= n
+    end
+    result
+  end
+
+  def drop_while
+    return to_enum(:drop_while) unless block_given?
+    result = []
+    each do |element|
+      result << elemnet unless yield(elemnet)
+    end
+    result
+  end
+
+alias collect map
+alias select filter
+alias foldl reduce
 end
--- week1/3-my-enumerable/solution_test.rb
+++ week1/3-my-enumerable/solution_test.rb
@@ -18,30 +18,147 @@ class SolutionTest < Minitest::Test
   def test_map
     collection = Collection.new(*1..5)
 
-    assert_equals [2, 3, 4, 5, 6], collection.map(&:succ)
+    assert_equal [2, 3, 4, 5, 6], collection.map(&:succ)
+    assert_equal true, collection.map.is_a?(Enumerator)
   end
 
   def test_filter
     collection = Collection.new(*1..10)
 
-    assert_equals [1, 3, 5, 7, 9], collection.filter(&:odd?)
+    assert_equal [1, 3, 5, 7, 9], collection.filter(&:odd?)
+    assert_equal true, collection.filter.is_a?(Enumerator)
   end
 
   def test_reject
     collection = Collection.new(*1..10)
 
-    assert_equals [1, 3, 5, 7, 9], collection.reject(&:even?)
+    assert_equal [1, 3, 5, 7, 9], collection.reject(&:even?)
+    assert_equal true, collection.reject.is_a?(Enumerator)
   end
 
   def test_reduce
     collection = Collection.new(*1..10)
 
-    assert_equals 55, collection.reduce(0) { |sum, n| sum + n }
+    assert_equal 55, collection.reduce(0) { |sum, n| sum + n }
   end
 
   def test_include?
     collection = Collection.new(*1..10)
 
-    assert_equals true, collection.include?(5)
+    assert_equal true, collection.include?(5)
   end
+
+  def test_any?
+    collection = Collection.new(%w[ant bear cat])
+
+    assert_equal true, collection.any? { |word| word.length >= 3 }
+  end
+
+  def test_one?
+    collection = Collection.new(*%w[ant bear cat])
+
+    assert_equal true, collection.one? { |word| word.length == 4 }
+  end
+
+  def test_all?
+    collection = Collection.new(%w[ant bear cat])
+
+    assert_equal false, collection.all? { |word| word.length == 4 }
+  end
+
+  def test_count(element = nil)
+    collection = Collection.new(1, 2, 4, 2)
+
+    assert_equal 4, collection.count
+    assert_equal 2, collection.count(2)
+    assert_equal 3, collection.count { |x| x%2==0 }
+  end
+
+  def test_size
+  collection = Collection.new(1, 2, 4, 2)
+
+  assert_equal 4, collection.size
+  end
+
+  def test_group_by
+    collection = Collection.new(*1..6)
+
+    assert_equal collection.group_by { |i| i%3 },
+                {0=>[3, 6], 1=>[1, 4], 2=>[2, 5]}
+    assert_equal true, collection.group_by.is_a?(Enumerator)
+  end
+
+  def test_min
+    collection = Collection.new(*1..10)
+    word_collection = %w(albatross dog horse)
+
+    assert_equal 'dog', word_collection.min { |a, b| a.length <=> b.length }
+    assert_equal 1, collection.min
+  end
+
+  def test_min_by
+    collection = %w(albatross dog horse)
+
+    assert_equal 'dog', collection.min_by(&:length)
+  end
+
+  def test_max
+    collection = Collection.new(*1..10)
+
+    word_collection = %w(albatross dog horse)
+    assert_equal 'albatross', word_collection.max{|a, b| a.length <=> b.length}
+    assert_equal 10, collection.max
+  end
+
+  def test_max_by
+    collection = Collection.new(*1..10)
+
+    word_collection = %w(albatross dog horse)
+    assert_equal 'albatross', word_collection.max_by(&:length)
+    assert_equal true, collection.max_by.is_a?(Enumerator)
+  end
+
+  def test_minmax
+    collection = Collection.new(*1..10)
+
+    word_collection = %w(albatross dog horse)
+    assert_equal %w(dog albatross),
+                 word_collection.minmax { |a, b| a.length <=> b.length }
+    assert_equal [1, 10], collection.minmax
+  end
+
+  def test_minmax_by
+    collection = Collection.new(*1..10)
+
+    word_collection = %w(albatross dog horse)
+    assert_equal %w(dog albatross), word_collection.minmax_by(&:length)
+    assert_equal true, collection.minmax_by.is_a?(Enumerator)
+  end
+
+  def test_take
+    collection = Collection.new(*1..10)
+
+    assert_equal [1, 2, 3], collection.take(3)
+  end
+
+  def test_take_while
+    collection = [1, 2, 3, 4, 5, 0]
+
+    assert_equal [1, 2], collection.take_while { |i| i < 3 }
+    assert_equal true, collection.take_while.is_a?(Enumerator)
+  end
+
+  def test_drop
+    collection = Collection.new(*1..10)
+
+    assert_equal [9, 10], collection.drop(8)
+  end
+
+  # def test_drop_while
+  #   collection = [1, 2, 3, 4, 5, 0]
+
+  #   assert_equal [3, 4, 5, 0], collection.drop_while { |i| i < 3 }
+  #   assert_equal true, collection.drop_while.is_a?(Enumerator)
+  # end
+
 end
